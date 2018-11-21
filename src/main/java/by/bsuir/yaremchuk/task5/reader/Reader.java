f16213900c3074c0443adc54c4c32c74fec9af76
Igor Yaremchuk <yaremasony@gmail.com>
2018-11-22 01:24:18 +0300

    task6 done

--- src/main/java/by/bsuir/yaremchuk/task5/reader/Reader.java
+++ src/main/java/by/bsuir/yaremchuk/task5/reader/Reader.java
@@ -1,60 +1,22 @@
 package by.bsuir.yaremchuk.task5.reader;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
-public abstract class Reader {
-    public static List<Integer> readArray() {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> inputArray = new ArrayList<Integer>();
+public class Reader {
 
-        while (true) {
-            String line = scanner.nextLine();
-            if (line.length() < 1) {
-                break;
-            }
-            String[] numbers = line.split(" ");
-            for (String num : numbers) {
-                int intValue = Integer.parseInt(num);
-                inputArray.add(intValue);
-            }
+    public static int[] readArray(int size) {
+        int[] result = new int[size];
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < size; i++) {
+            result[i] = input.nextInt();
         }
 
-
-        return inputArray;
+        return result;
     }
+
+
 
 }
 
-class A {
-    static {
-        System.out.print("static-A, ");
-    }
-    {
-        System.out.print("init-A, ");
-    }
-    public A() {
-        System.out.print("constructor-A, ");
-    }
-}
-
-class B extends A {
-    static {
-        System.out.print("static-B, ");
-    }
-    {
-        System.out.print("init-B, ");
-    }
-    public B(){
-        System.out.print("Constructor-B, ");
-    }
-}
-
-class Test {
-    public static void main(String[] args) {
-        new B();
-
-    }
-}
